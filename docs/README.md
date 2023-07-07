6936f08dc36a781dcc087c2630ff783c81516125
Jakub Andrysek <jakub.andrysek03@gmail.com>
2023-07-07 10:14:41 +0200

    Add hashing functions

--- docs/README.md
+++ docs/README.md
@@ -26,16 +26,18 @@ plugins:
         - myCode
         - toShare
         - folderX/thisWillBeZipped
+      hash_extension: ".zip.hash" # default extension
       debug: true # optional - default is false
 ```
 
 - `folders` - A list of folders to zip. The folders are relative to the root of the MkDocs documentation project - docs_dir (default is `docs`).
 - `debug` - Optional. If set to `true`, the plugin will print debug messages to the console. Default is `false`.
+- `hash_extension` - The extension to use for hashing folders. Default is `.zip.hash`.
 
 
 ## Git configuration
 
-It is highly recommended to add the zip files to your `.gitignore` file. This will prevent you from accidentally committing the zip files to your repository.
+It is highly recommended to add the `*.zip`, `*.zip.hash` filses to your `.gitignore` file. This will prevent you from accidentally committing the zip files to your repository.
 
 Zip files will be generated automatically after each build.
 
--- zip_folders/plugin.py
+++ zip_folders/plugin.py
@@ -4,8 +4,10 @@
 # License: MIT
 # GitHub: https://github.com/JakubAndrysek/mkdocs-typedoc
 # PyPI: https://pypi.org/project/mkdocs-typedoc/
+
 import os
 import zipfile
+import hashlib
 import logging
 
 from mkdocs.plugins import BasePlugin
@@ -14,10 +16,12 @@ from mkdocs.structure.files import File
 
 log: logging.Logger = logging.getLogger("mkdocs")
 
+
 class ZipFoldersPlugin(BasePlugin):
     config_scheme = (
         ('folders', config_options.Type(list, default=[])),
         ('debug', config_options.Type(bool, default=False)),
+        ('hash_extension', config_options.Type(str, default='.zip.hash')),
     )
 
     def on_post_build(self, config):
@@ -29,7 +33,25 @@ class ZipFoldersPlugin(BasePlugin):
                 print(f"The folder {folder} does not exist.")
                 continue
 
+            zip_path = f'{path}.zip'
+            hash_path = f'{path}{self.config["hash_extension"]}'
+
+            if os.path.exists(zip_path) and os.path.exists(hash_path):
+                with open(hash_path, 'r') as f:
+                    old_hash = f.read().strip()
+
+                new_hash = self.hash_file(zip_path)
+
+                if old_hash == new_hash:
+                    if self.config['debug']:
+                        log.info(f"Skipping {folder} as it has not changed.")
+                    continue
+
             self.zip_folder(path, self.config['debug'])
+
+            # Compute and store new hash
+            with open(hash_path, 'w') as f:
+                f.write(self.hash_file(zip_path))
 
         return config
 
@@ -47,3 +69,17 @@ class ZipFoldersPlugin(BasePlugin):
 
         if debug:
             log.info(f"Zipped {folder} into {folder}.zip")
+
+    @staticmethod
+    def hash_file(filepath):
+        BUF_SIZE = 65536  # Let's read stuff in 64kb chunks!
+        sha256 = hashlib.sha256()
+
+        with open(filepath, 'rb') as f:
+            while True:
+                data = f.read(BUF_SIZE)
+                if not data:
+                    break
+                sha256.update(data)
+
+        return sha256.hexdigest()
